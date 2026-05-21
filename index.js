e7f3cedd3fbb2347b5b04d2937ee888de16589fe
Muhatarima <muhatarima@gmail.com>
2026-05-21 18:05:50 +0600

    connect mongodb database with server

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-
+// middleware
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Pet Adoption Server Running");
+});
+
+// server run
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
