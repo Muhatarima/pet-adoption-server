1fa917d9ebce1221a2094fc7615de2c2bb4d7179
Muhatarima <muhatarima@gmail.com>
2026-05-21 18:00:42 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.get("/", (req, res) => {
+  res.send("Pet Adoption Server Running");
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
