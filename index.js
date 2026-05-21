b59873d2545827c2be6870cf738033096e454d2b
Muhatarima <muhatarima@gmail.com>
2026-05-21 18:03:06 +0600

    middleware setup

--- index.js
+++ index.js
@@ -10,13 +10,3 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-
-
-app.get("/", (req, res) => {
-  res.send("Pet Adoption Server Running");
-});
-
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
