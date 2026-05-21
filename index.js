7fd00cc9523483c4f3ceba720520983630e3bb30
Muhatarima <muhatarima@gmail.com>
2026-05-21 19:48:34 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iuqwjlp.mongodb.net/?appName=Cluster0`;
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
@@ -16,6 +24,16 @@ app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send("Pet Adoption Server Running");
 });
+async function run() {
+  try {
+    await client.connect();
+
+    console.log("MongoDB connected successfully");
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 
 app.listen(port, () => {
