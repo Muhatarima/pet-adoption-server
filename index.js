5bb2f5ac09f8ae3bb4cc427945740c0f6e76448d
Muhatarima <muhatarima@gmail.com>
2026-05-21 19:28:31 +0600

    mongodb atlas created

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iuqwjlp.mongodb.net/?appName=Cluster0`;
 
-// middleware
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-// test route
+
 app.get("/", (req, res) => {
   res.send("Pet Adoption Server Running");
 });
 
-// server run
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
